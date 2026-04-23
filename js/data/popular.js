b3328da280d659b3aab9354cf8a02892281e901b
wldbs5165 <wldbs5165@naver.com>
2026-04-23 17:23:12 +0900

    design: 추천화면 디자인, 구현 완료, 홈화면 슬라이드 하는 중

---
+++ js/data/popular.js
@@ -0,0 +1,30 @@
+export const popularData = [
+  {
+    thumbnail: "assets/images/방콕-상세.png",
+    alt: "방콕 이미지",
+    location: "방콕",
+    sub: "저렴한 물가와 화려한 밤이 살아있는 여행지",
+    tags: ["가성비", "야시장", "먹방"],
+  },
+  {
+    thumbnail: "assets/images/다낭-상세.png",
+    alt: "다낭 이미지",
+    location: "다낭",
+    sub: "휴양과 액티비티를 한 번에 즐기는 가성비 여행지",
+    tags: ["휴양", "가성비", "액티비티"],
+  },
+  {
+    thumbnail: "assets/images/도쿄-상세.png",
+    alt: "도쿄 이미지",
+    location: "도쿄",
+    sub: "트랜드와 전통이 공존하는 매력적인 대도시",
+    tags: ["도시", "쇼핑", "먹방"],
+  },
+  {
+    thumbnail: "assets/images/타이베이-상세.png",
+    alt: "타이베이 이미지",
+    location: "타이베이(대만)",
+    sub: "다양한 먹거리와 편리한 대중교통",
+    tags: ["먹방", "야시장", "대중교통"],
+  },
+];
---
+++ js/module/card.js
@@ -0,0 +1,49 @@
+import { cardData } from "./../data/card.js";
+
+// 카드 들어갈 영역 불러옴
+const cardContainer = document.querySelector(".card-container");
+
+// -----카드 출력-----
+function renderCards() {
+  cardContainer.innerHTML = cardData
+    .map(
+      (item) => `<div class="card">
+
+                <div class="card-thumbnail">
+                  <img
+                    src="${item.thumbnail}"
+                    alt="${item.alt}"
+                  />
+                </div>
+                <div class="card-name">
+                  <img src="assets/icons/map-pin.png" alt="위치 아이콘" />
+                  <h3 class="card-location">${item.location}</h3>
+                </div>
+                <span class="card-sub"
+                  >""${item.sub}""</span
+                >
+                <div class="card-tag">
+                  ${item.tags.map((tag) => `<span>@ ${tag}</span>`).join("")}
+                </div>
+                <div class="card-actions">
+                  <button class="save-btn">
+                    <img
+                      src="assets/icons/bookmark.png"
+                      alt="저장하기 아이콘"
+                    />
+                    <span class="caption">저장하기</span>
+                  </button>
+                  <a href="#" class="share-btn">
+                    <img
+                      src="assets/icons/external-link.png"
+                      alt="공유하기 아이콘"
+                    />
+                    <span class="caption">공유하기</span>
+                  </a>
+                </div>
+              </div>`,
+    )
+    .join("");
+}
+
+renderCards();
